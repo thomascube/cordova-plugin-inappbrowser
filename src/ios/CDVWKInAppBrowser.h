497fe356e9890a64dd912ba9774abd40469f7e27
Thomas Bruederli <t.bruederli@nexbyte.com>
2020-07-24 13:55:39 +0200

    Add missing property `toolbarAddressLabel` to header file

--- src/ios/CDVWKInAppBrowser.h
+++ src/ios/CDVWKInAppBrowser.h
@@ -59,6 +59,7 @@
 @property (nonatomic, strong) IBOutlet WKWebViewConfiguration* configuration;
 @property (nonatomic, strong) IBOutlet UIBarButtonItem* closeButton;
 @property (nonatomic, strong) IBOutlet UILabel* addressLabel;
+@property (nonatomic, strong) IBOutlet UILabel* toolbarAddressLabel;
 @property (nonatomic, strong) IBOutlet UIBarButtonItem* backButton;
 @property (nonatomic, strong) IBOutlet UIBarButtonItem* forwardButton;
 @property (nonatomic, strong) IBOutlet UIActivityIndicatorView* spinner;
